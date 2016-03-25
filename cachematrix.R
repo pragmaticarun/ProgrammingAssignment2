06b0b6faa343273a8e119305a7cd9e3f7b9fe67b
Arunkumar M R <pragmaticarun@gmail.com>
2016-03-25 18:27:11 +0530

    Added code for creating a matrix object which can hold its inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+#author Arunkumar Maniam Rajan
 
-## Write a short comment describing this function
+##  This R program contains functions which creates a special matrix object which can cache the inverse.
+
+## This function provides set/get methods for modifying the matrix and provides setInverse/getInverse functions to set/get the cache which holds the Inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
+      cachedInverse <- NULL
+      set <- function(newMatrix) {
+         x <<- newMatrix
+         cachedInverse <<- NULL #Needs to be done if a Matrix changed
+      }
 
+      get <- function() {
+         x
+      }
+
+      setInverse <- function(inverse) {
+         cachedInverse <<- inverse
+      }
+
+      getInverse <- function() {
+         cachedInverse
+      }
+
+      list( set = set, get = get,
+           setInverse = setInverse,
+           getInverse = getInverse )
+         
 }
 
 
-## Write a short comment describing this function
+## This function caches the result of inverse operation in the 
+## object passed which can store the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   i = x$getInverse()
+   if(!is.null(i)) {
+      message("Getting cached Inverse")
+      return(i)
+   }
+
+   inputMatrix = x$get()
+   i <- solve(inputMatrix, ...)
+   x$setInverse(i)
+   i
 }
+
+## Test function to generate invertible matrix
+hilbert <- function(n){ 
+   i <- 1:n; 1 / outer(i - 1, i, "+") 
+}
+
